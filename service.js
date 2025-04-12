44e623f15d649c041a001cfe5e11af74a05fb2c0
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-04-12 15:10:48 +0200

    logs for service

--- service.js
+++ service.js
@@ -1,14 +1,17 @@
 const Service = require('node-windows').Service
-
+const path = require('path')
 
 const svc = new Service({
   name: 'MailBridgeBot',
   description: 'Telegram bot for MailBridge',
-  script: require('path').join(__dirname, 'index.js'),
+  script: path.join(__dirname, 'index.js'),
   nodeOptions: [
     '--harmony',
     '--max_old_space_size=4096'
-  ]
+  ],
+  logpath: path.join(__dirname, 'logs'),
+  logfile: path.join(__dirname, 'logs', 'service.log'),
+  errorfile: path.join(__dirname, 'logs', 'error.log')
 })
 
 svc.on('install', () => {
@@ -32,11 +35,9 @@ svc.on('stop', () => {
   console.log('Service stopped.')
 })
 
-
 if (process.argv.includes('--install')) {
   svc.install()
 }
-
 
 if (process.argv.includes('--uninstall')) {
   svc.uninstall()
